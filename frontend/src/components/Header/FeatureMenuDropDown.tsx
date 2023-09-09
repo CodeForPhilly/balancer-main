635903dd57f6beccaabe68d858ed86035de3a61c
taichan03 <tai.c.chan3@gmail.com>
2023-09-09 09:58:24 -0400

    menu link changes

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import { useState, useCallback } from "react";
+import SearchMenu from "./SearchMenu";
 
 export const FeatureMenuDropDown = () => {
+  const [showSearchMenu, setShowSearchMenu] = useState(false);
+
+  const handleSearchMenu = useCallback(() => {
+    setShowSearchMenu((prev) => !prev);
+  }, []);
+
   return (
     <div className="xl:px-50 absolute left-0 right-0 top-full mt-0 flex h-72 w-full flex-row rounded bg-white px-4 py-2 font-inter shadow-lg sm:px-6 md:px-8 lg:px-8 2xl:px-56">
       <div className="  mr-26 w-28 "> </div>
@@ -15,26 +23,23 @@ export const FeatureMenuDropDown = () => {
         </ul>
         <ul className=" mb-8">
           <Link
-            to="/login"
+            to="/drugSummary"
             className=" mr-9 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
           >
             Medication Summary and Comparison
-            {/* <span className="absolute ml-1.5 transition-transform duration-300 hover:rotate-180">
-            &#8593;
-          </span> */}
           </Link>
         </ul>
-        <ul className=" mb-8">
-          <Link
-            to="/login"
-            className="mr-9 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-          >
-            Review Lookup
-            {/* <span className="absolute ml-1.5 transition-transform duration-300 hover:rotate-180">
-            &#8593;
-          </span> */}
-          </Link>
-        </ul>
+
+        {/* <div
+          className="mr-9 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
+          onClick={handleSearchMenu}
+        >
+          <span className="flex-auto"> Review Lookup</span>
+        </div>
+        <SearchMenu
+          showSearchMenu={showSearchMenu}
+          handleSearchMenu={handleSearchMenu}
+        /> */}
       </div>
     </div>
   );

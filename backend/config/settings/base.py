a4320a2eb2b8acd6ce441dc62f4001a78d37397d
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-09-09 16:12:02 -0400

    chore: add auth settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -40,11 +40,19 @@ DJANGO_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
 ]
 FIRST_PARTY_APPS = [
-    "users",
+    "balancer.users",
 ]
-INSTALLED_APPS = DJANGO_APPS + FIRST_PARTY_APPS
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
+]
+
+INSTALLED_APPS = DJANGO_APPS + FIRST_PARTY_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -131,4 +139,26 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Authentication
+DJOSER = {
+    "LOGIN_FIELD": "email",
+    "SERIALIZERS": {"user_create": "balancer.users.serializers.UserCreateSerializer"},
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    )
+}
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
 AUTH_USER_MODEL = "users.User"
+ACCOUNT_EMAIL_REQUIRED = True  # Required for email auth
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_USERNAME_REQUIRED = False
+
+SITE_ID = 1

38ca871a2a046de9e10dbc8d45731cc9ac0edebd
taichan03 <tai.c.chan3@gmail.com>
2023-09-10 10:28:26 -0400

    center links

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -11,7 +11,8 @@ export const FeatureMenuDropDown = () => {
 
   return (
     <div className="xl:px-50 absolute left-0 right-0 top-full mt-0 flex h-72 w-full flex-row rounded bg-white px-4 py-2 font-inter shadow-lg sm:px-6 md:px-8 lg:px-8 2xl:px-56">
-      <div className="  mr-26 w-28 "> </div>
+      <div className="  mr-6 w-72 "> </div>
+      <div className="  mr-6 w-72 "> </div>
       <div className="">
         <ul className=" mb-8 mt-8">
           <Link

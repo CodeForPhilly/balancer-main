095a053f954e6e50b3457e1a4446cc3ad2353406
taichan03 <tai.c.chan3@gmail.com>
2023-09-12 23:30:13 -0400

    update header

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -1,14 +1,6 @@
 import { Link } from "react-router-dom";
-import { useState, useCallback } from "react";
-import SearchMenu from "./SearchMenu";
 
 export const FeatureMenuDropDown = () => {
-  const [showSearchMenu, setShowSearchMenu] = useState(false);
-
-  const handleSearchMenu = useCallback(() => {
-    setShowSearchMenu((prev) => !prev);
-  }, []);
-
   return (
     <div className="xl:px-50 absolute left-0 right-0 top-full mt-0 flex h-72 w-full flex-row rounded bg-white px-4 py-2 font-inter shadow-lg sm:px-6 md:px-8 lg:px-8 2xl:px-56">
       <div className="  mr-6 w-72 "> </div>

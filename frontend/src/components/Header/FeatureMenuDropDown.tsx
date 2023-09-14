d3aea3460aa704345b24b9c93976e6b08ab7d5d3
Tai Chan <tai.c.chan3@gmail.com>
2023-09-14 14:52:54 -0400

    header changes

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom";
 
 export const FeatureMenuDropDown = () => {
   return (
-    <div className="xl:px-50 absolute left-0 right-0 top-full mt-0 flex h-72 w-full flex-row rounded bg-white px-4 py-2 font-inter shadow-lg sm:px-6 md:px-8 lg:px-8 2xl:px-56">
-      <div className="  mr-6 w-72 "> </div>
-      <div className="  mr-6 w-72 "> </div>
+    <div className="xl:px-50 absolute   justify-center top-full mt-0 flex h-72 w-[50%] flex-row rounded bg-white px-4 py-2 font-inter shadow-lg sm:px-6 md:px-8 lg:px-8 2xl:px-56">
+
       <div className="">
         <ul className=" mb-8 mt-8">
           <Link

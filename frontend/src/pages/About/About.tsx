3675588cc8e2cbb473e05d8abb3d47724923d165
taichan03 <tai.c.chan3@gmail.com>
2023-09-13 20:48:39 -0400

    set up the layout for development

---
+++ frontend/src/pages/About/About.tsx
@@ -0,0 +1,14 @@
+import Welcome from "../../components/Welcome/Welcome.tsx";
+import Layout from "../Layout/Layout";
+
+function About() {
+  return (
+    <Layout>
+      <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
+        <div className="mt-10">test</div>
+      </div>
+    </Layout>
+  );
+}
+
+export default About;

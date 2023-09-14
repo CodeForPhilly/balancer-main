290d09a658ef840635414f78d4f1bb8a7243052a
taichan03 <tai.c.chan3@gmail.com>
2023-09-14 05:53:08 -0400

    updating text

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <Layout>
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
-        <div className="mt-10">test</div>
+        <div className="mt-10">This is layout for about page</div>
       </div>
     </Layout>
   );

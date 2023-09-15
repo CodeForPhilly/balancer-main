4770f4d37187087981e97ce29c46922d1194a8f3
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-09-15 19:08:49 -0400

    added form basics

---
+++ frontend/src/pages/Feedback/FeedbackForm.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  message: Yup.string().required('Message is required'),
+});
+
+const FeedbackForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      // Handle form submission logic here
+      console.log('Submitted data:', values);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          {...formik.getFieldProps('name')}
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div>{formik.errors.name}</div>
+        ) : null}
+      </div>
+
+      {/* Similar code for email and message fields */}
+      
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FeedbackForm;

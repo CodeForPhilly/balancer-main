b8eff054b6b37b549189a17f5fecf35a1d79d449
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-09-16 16:04:59 -0400

    skelton html codes in

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -5,7 +5,39 @@ function About() {
   return (
     <Layout>
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
-        <div className="mt-10">This is layout for about page</div>
+        {/* Top section */}
+        <div className="mt-10 top-section flex w-full max-w-6xl flex-row items-center justify-center md:mt-28">
+          <div className="flex flex-col items-center justify-center">
+            <div className="text-6xl">[Placeholder] We are helping bipolar patients to manage their medication.</div>
+            <div className="text-lg mt-5 text-gray-500">[Placeholder] paragrah of what we do</div>
+          </div>
+          <div>
+            [Placeholder]image here
+          </div>
+        </div>
+
+        {/* Mission section */}
+        <div className="mt-10 mission-section flex w-full max-w-6xl flex-row items-center justify-center md:mt-28">
+          <div className="flex flex-col items-center justify-center">
+            <div className="text-3xl">Our mission</div>
+            <div className="text-lg mt-5 text-gray-500">[Placeholder] paragrah of our mission</div>
+          </div>
+          <div>
+            [Placeholder]some stats
+          </div>
+        </div>
+
+        {/* Team section */}
+        <div className="mt-10 team-section flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
+          <div className="text-3xl">Our team</div>
+          <div className="people-section-container">
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );

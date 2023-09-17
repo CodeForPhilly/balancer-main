364bf86e15515df8772403ddd84aad6675f541f6
Tai Chan <tai.c.chan3@gmail.com>
2023-09-17 19:17:15 -0400

    update header menu

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 
 export const FeatureMenuDropDown = () => {
   return (
-    <div className="absolute text-sm items-center top-full mt-0 flex h-40 w-90 flex-row rounded-lg bg-white font-inter shadow-xl shadow-blue-300/40 sm:px-6 md:px-8 lg:px-8 xl:px-6 ">
+    <div className="absolute text-sm items-center mt-2 top-full mt-0 flex h-40 w-90 flex-row rounded-lg bg-white font-inter border-2 sm:px-6 md:px-8 lg:px-8 xl:px-6 ">
 
       <div className="mx-5">
         <ul className=" mb-2 mt-2">
           <Link
             to="/"
-            className=" text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
+            className=" text-black font-bold hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
           >
             Medication Suggester
           </Link>
+          <div className="text-sm font-satoshi mt-1 text-gray-400">Medication recommendations based on symptoms and medical history.</div>
         </ul>
-        <ul className=" mb-2">
+        <ul className=" mb-2 mt-5">
           <Link
             to="/drugSummary"
             className=" mr-9 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"

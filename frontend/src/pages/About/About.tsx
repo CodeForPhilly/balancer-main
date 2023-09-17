ae4cc520ad83e94ad2a3194582c8f69172b8cf40
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-09-17 10:53:51 -0400

    update layout

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -17,20 +17,47 @@ function About() {
         </div>
 
         {/* Mission section */}
-        <div className="mt-10 mission-section flex w-full max-w-6xl flex-row items-center justify-center md:mt-28">
-          <div className="flex flex-col items-center justify-center">
-            <div className="text-3xl">Our mission</div>
-            <div className="text-lg mt-5 text-gray-500">[Placeholder] paragrah of our mission</div>
-          </div>
-          <div>
-            [Placeholder]some stats
+        <div className="mt-10 mission-section flex w-full max-w-6xl flex-col items-start justify-center md:mt-28">
+          <div className="text-3xl">Our mission</div>
+          <div className="flex flex-row justify-between w-full">
+            <div className="text-lg mt-5 text-gray-500">
+              [Placeholder] paragrah of our mission
+            </div>
+            <div className="text-lg mt-5 text-gray-500">
+              [Placeholder]some stats
+            </div>
           </div>
         </div>
 
         {/* Team section */}
-        <div className="mt-10 team-section flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
+        <div className="mt-10 team-section flex w-full max-w-6xl flex-col items-start md:mt-28">
           <div className="text-3xl">Our team</div>
-          <div className="people-section-container">
+          <div className="people-section-container flex flex-row mt-5 w-full justify-between">
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
+            <div>
+              <div>Picture</div>
+              <div>Name</div>
+              <div>Role</div>
+            </div>
             <div>
               <div>Picture</div>
               <div>Name</div>

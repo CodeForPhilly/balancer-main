1002b2f9680c820509ed8c88707927755e238ac7
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-02 16:04:53 -0400

    added border

--- frontend/src/pages/Feedback/FeedbackForm.tsx
+++ frontend/src/pages/Feedback/FeedbackForm.tsx
@@ -76,77 +76,79 @@ const FeedbackForm = () => {
     <>
       <section className="mx-auto mt-12 w-full max-w-xs">
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="mb-2 block text-sm font-bold text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              onChange={handleChange}
-              value={values.name}
-              className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
-            />
-            <div className="form-error-container">
-              {touched.name && errors.name && (
-                <p className="text-sm text-red-500">{errors.name}</p>
-              )}
+          <div className="summary_box">
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="mb-2 block text-sm font-bold text-gray-700"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={handleChange}
+                value={values.name}
+                className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
+              />
+              <div className="form-error-container">
+                {touched.name && errors.name && (
+                  <p className="text-sm text-red-500">{errors.name}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="mb-2 block text-sm font-bold text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              value={values.email}
-              className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
-            />
-            <div className="form-error-container">
-              {touched.email && errors.email && (
-                <p className="text-sm text-red-500">{errors.email}</p>
-              )}
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="mb-2 block text-sm font-bold text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                onChange={handleChange}
+                value={values.email}
+                className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
+              />
+              <div className="form-error-container">
+                {touched.email && errors.email && (
+                  <p className="text-sm text-red-500">{errors.email}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="message"
-              className="mb-2 block text-sm font-bold text-gray-700"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              onChange={handleChange}
-              value={values.message}
-              rows={8}
-              className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
-            />
-            <div className="form-error-container">
-              {touched.message && errors.message && (
-                <p className="text-sm text-red-500">{errors.message}</p>
-              )}
+            <div className="mb-4">
+              <label
+                htmlFor="message"
+                className="mb-2 block text-sm font-bold text-gray-700"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                onChange={handleChange}
+                value={values.message}
+                rows={8}
+                className={` focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none disabled:bg-gray-200`}
+              />
+              <div className="form-error-container">
+                {touched.message && errors.message && (
+                  <p className="text-sm text-red-500">{errors.message}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-end">
-            <button
-              className="black_btn disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-600"
-              type="submit"
-              disabled={isLoading}
-            >
-              Submit
-            </button>
+            <div className="flex items-center justify-end">
+              <button
+                className="black_btn disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-600"
+                type="submit"
+                disabled={isLoading}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </form>
       </section>

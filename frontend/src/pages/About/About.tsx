dd1e55024d0ab2c2617884eb48d860267d8ae8aa
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-10-05 22:58:55 -0400

    add contents in

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -7,9 +7,13 @@ function About() {
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
         {/* Top section */}
         <div className="mt-10 top-section flex w-full max-w-6xl flex-row items-center justify-center md:mt-28">
-          <div className="flex flex-col items-center justify-center">
-            <div className="text-5xl">[Placeholder] We are helping bipolar patients to manage their medication.</div>
-            <div className="text-lg mt-5 text-gray-500">[Placeholder] paragrah of what we do</div>
+          <div className="flex flex-col justify-center">
+            <div className="text-5xl">Balancer</div>
+            <div className="text-3xl mt-5">A tool that makes it easier to research medications for bipolar disorder.</div>
+            <div className="text-lg mt-5 text-gray-500">It can take two to 10 years—and three to 30 medications—for people with bipolar disorder to find the right medication combination. Balancer is designed to help physicians shorten this journey for their patients.</div>
+            <div className="text-3xl mt-5">Get accurate, helpful information on bipolar medications fast</div>
+            <div className="text-lg mt-5 text-gray-500">Powered by innovative AI technology, Balancer is a tool that aids in providing personalized medication recommendations for patients with bipolar disorder in any state, including mania, depression, hypomania and mixed. Our platform utilizes machine learning to give you the latest, most up-to-date information on medications and active clinical trials to treat bipolar disorder. </div>
+            <div className="text-lg mt-5 text-gray-500">Balancer automates medication decision support by offering tailored medication recommendations and comprehensive risk-benefit assessments based on a patient's diagnosis, symptom severity, treatment goals and individual characteristics.  </div>
           </div>
           <div>
             [Placeholder]image here
@@ -19,50 +23,44 @@ function About() {
         {/* Mission section */}
         <div className="mt-10 mission-section flex w-full max-w-6xl flex-col items-start justify-center md:mt-28">
           <div className="text-3xl">Our mission</div>
-          <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-col justify-between w-full">
             <div className="text-lg mt-5 text-gray-500">
-              [Placeholder] paragrah of our mission
+              Bipolar disorder affects approximately 5.7 million adult Americans
+              <a href="https://www.dbsalliance.org/education/bipolar-disorder/bipolar-disorder-statistics/" className="underline" target="_blank"> every year</a>.
+              Delays in the correct diagnosis and proper treatment of bipolar disorder may result in social, occupational, and economic burdens, as well as
+              <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2796048/" className="underline" target="_blank"> an increase in completed suicides</a>.
             </div>
             <div className="text-lg mt-5 text-gray-500">
-              [Placeholder]some stats
+              The team behind Balancer believes that building a searchable, interactive and user-friendly research tool for bipolar medications has the potential to improve the health and well-being of people with bipolar disorder.
             </div>
           </div>
         </div>
 
-        {/* Team section */}
-        <div className="mt-10 team-section flex w-full max-w-6xl flex-col items-start md:mt-28">
-          <div className="text-3xl">Our team</div>
-          <div className="people-section-container flex flex-row mt-5 w-full justify-between">
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
-            <div>
-              <div>Picture</div>
-              <div>Name</div>
-              <div>Role</div>
-            </div>
+        {/* Support Us section */}
+        <div className="mt-10 team-section flex w-full max-w-6xl flex-col items-center md:mt-28">
+          <div className="flex w-full items-start">
+            <div className="text-3xl items-start">Support Us</div>
+          </div>
+          <div className="text-lg mt-5 text-gray-500">
+            Balancer is led by a passionate team of volunteer developers, coders, UX designers, data scientists and researchers. Our vision is to create digital tools that support physicians in choosing the most suitable medication for patients with bipolar disorder. We also aim to support patients in their journey to medication stability, enabling them to be more informed and therefore more involved in medication decision-making.
+          </div>
+          <div className="text-lg mt-5 text-gray-500">
+            Balancer is a not-for-profit, civic-minded, open-source project sponsored by
+            <a href="https://codeforphilly.org/" className="underline" target="_blank"> Code for Philly</a>
+            . We are currently seeking donations to fund the next phase of development. Your contribution will go towards website domains and hosting, helpdesk software and marketing efforts.
+          </div>
+
+          <div className="flex flex-row flex-wrap mt-5 mb-20">
+            <button
+              className="btn transition-transform focus:outline-none focus:ring focus:ring-blue-200 mr-5"
+            >
+              Donate Here
+            </button>
+            <button
+              className="btn transition-transform focus:outline-none focus:ring focus:ring-blue-200"
+            >
+              Contact Us
+            </button>
           </div>
         </div>
       </div>

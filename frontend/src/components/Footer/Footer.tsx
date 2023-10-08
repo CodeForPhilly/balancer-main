9a0880119184693ff6578d11a1fea8f89dfa0c9b
taichan03 <tai.c.chan3@gmail.com>
2023-10-08 14:25:49 -0400

    commit for mandy

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,86 +1,68 @@
-import React, { useState } from "react";
-// import "./footer.css"; // Import the corresponding CSS file
-import "../../components/Footer/footer.css";
+// Footer.js
+
+import { useState } from "react";
 import { Link } from "react-router-dom";
-const Footer = () => {
+import "../../App.css"; // Import the common Tailwind CSS styles
+
+function Footer() {
   const [isPressed, setIsPressed] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const handleMouseDown = () => {
-    setIsPressed(true);
-  };
-
-  const handleMouseUp = () => {
-    setIsPressed(false);
-  };
+  const handleMouseDown = () => setIsPressed(true);
+  const handleMouseUp = () => setIsPressed(false);
 
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          Copyright
-        </Link>
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          Contact us
-        </Link>
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          About
-        </Link>
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          Privacy policy
-        </Link>
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          Terms of use
-        </Link>
-        <Link
-          to="/login"
-          className="mr-5  text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
-        >
-          Leave feedback
-        </Link>
-      </div>
-      <div className="footer-form">
-        <input type="text" className="input" placeholder="Enter your email" />
-        <button
-          type="submit"
-          className={`btnBlue ${
-            isPressed &&
-            "transition-transform focus:outline-none focus:ring focus:ring-blue-200"
-          }${
-            isLoading
-              ? "bg-white-600 transition-transform focus:outline-none focus:ring focus:ring-blue-500"
-              : ""
-          }`}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-          disabled={isLoading} // Disable the button while loading
-        >
-          {isLoading ? (
-            <div className="flex items-center justify-center">
-              <div className="mr-2 h-4 w-4 animate-ping rounded-full bg-white"></div>
-              <p>Loading...</p>
-            </div>
-          ) : (
-            <p>Subscribe</p>
-          )}
-        </button>
-      </div>
-    </footer>
+    <div className="footer-content">
+      <footer className="footer">
+        {[
+          "Copyright",
+          "Contact us",
+          "About",
+          "Privacy policy",
+          "Terms of use",
+          "Leave feedback",
+        ].map((text, index) => (
+          <Link
+            key={index}
+            to="/login"
+            className="mr-5 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
+          >
+            {text}
+          </Link>
+        ))}
+
+        <div className="mt-6 flex items-center justify-center">
+          <div className="w-1/2 rounded border px-4 py-2 shadow-inner">
+            <input
+              type="email"
+              id="email"
+              className="focus:shadow-outline block w-full leading-tight text-gray-700 focus:outline-none"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="ml-4">
+            <button
+              type="submit"
+              className={`btnBlue ${
+                isPressed &&
+                "transition-transform focus:outline-none focus:ring focus:ring-blue-200"
+              }`}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+            >
+              {isPressed ? (
+                <div className="flex items-center justify-center">
+                  <div className="mr-2 h-4 w-4 animate-ping rounded-full bg-white"></div>
+                  <p>Loading...</p>
+                </div>
+              ) : (
+                <p>Subscribe</p>
+              )}
+            </button>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
-};
+}
+
 export default Footer;

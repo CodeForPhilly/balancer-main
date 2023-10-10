bc535c73e8aa4896c4ee96ebd16b321fed43c011
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-10-09 22:49:04 -0400

    formatting

--- backend/balancer/settings.py
+++ backend/balancer/settings.py
@@ -129,9 +129,11 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = False
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
 ]
+
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",

f865b782dff2ede55489a3211aa898529de6f8f9
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-10-10 20:00:51 -0400

    fix: prune authentication backend

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -152,8 +152,6 @@ REST_FRAMEWORK = {
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 AUTH_USER_MODEL = "users.User"

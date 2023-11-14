05216bd178d8c3eda46390404411aeb1d76c9419
taichan03 <tai.c.chan3@gmail.com>
2023-11-14 06:22:11 -0500

    endpoint

--- server/balancer_backend/views/listMeds/urls.py
+++ server/balancer_backend/views/listMeds/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from balancer_backend.views.listMeds import views
 
 urlpatterns = [
-    path("chatgpt/listMeds", listMeds.medication, name="listMeds")
+    path("chatgpt/listMeds", views.medication, name="listMeds")
 ]

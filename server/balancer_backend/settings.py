24faa04a011c874af0d12b0fd6e10f52f5fb3b8d
taichan03 <tai.c.chan3@gmail.com>
2023-11-19 13:45:19 -0500

    connecting frontend to backend

--- server/balancer_backend/settings.py
+++ server/balancer_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'registration',
     'balancer_backend',
     'user_management',
@@ -52,9 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'balancer_backend.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {

75a2ab9c499e6ec301e11ae384ee05f9334c8d14
Ryan Robert Rogers <ryan.rogers.developer@gmail.com>
2023-12-10 09:56:58 -0500

    serve with nginx and gunicorn

--- server/balancer_backend/settings.py
+++ server/balancer_backend/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 
-python manage.py flush --no-input
-python manage.py migrate
+# python manage.py flush --no-input
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Starting server..."
+gunicorn balancer_backend.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"

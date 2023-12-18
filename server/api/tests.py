aba46f334f3ad3cb9bf41b749db38794b5bf22ec
Erin Claudio <eclaudio@gmail.com>
2023-12-18 14:45:01 -0500

    Update first commit

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+from unittest.mock import patch
+import json
+class ChatGPTViewTest(TestCase):
+ 
+ def setUp(self):
+        self.client = Client()

abc72e005ca8d19d061e004f09a81a44f0a71b21
Ryan Robert Rogers <ryan.rogers.developer@gmail.com>
2023-12-17 19:35:13 -0500

    rename .env

--- server/create_app.py
+++ server/create_app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from django.core.management import execute_from_command_line
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '../config/env/.env.dev')
+dotenv_path = os.path.join(os.path.dirname(__file__), '../config/env/..env')
 load_dotenv(dotenv_path)
 
 execute_from_command_line(["manage.py", "startapp", "appname"])

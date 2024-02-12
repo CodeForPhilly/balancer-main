7ba8ea40b7aa907d622c1fab0db402fe4348602e
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-02-12 16:46:28 -0500

    formatting

--- frontend/src/pages/Help/Help.tsx
+++ frontend/src/pages/Help/Help.tsx
@@ -9,14 +9,10 @@ interface HelpData {
 }
 
 const HelpCardData: HelpData = {
-  icon: [
-    "UserDoctor",
-    "FeedbackIcon",
-    "MagnifyingGlassChart",
-  ],
+  icon: ["UserDoctor", "FeedbackIcon", "MagnifyingGlassChart"],
   title: ["How To Use this Site", "Submit Feedback", "How We Get Our Data"],
   paragraph: [
-      "Visit this page to learn how to use the Balancer App!",
+    "Visit this page to learn how to use the Balancer App!",
     "Give the Balancer team feedback on your experience.",
     "Learn where the Balancer team gets our data!",
   ],
@@ -43,15 +39,31 @@ function Help() {
     <Layout>
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center px-4 md:mt-28">
         <div className="flex justify-center ">
-          <h1 className="mt-20 text-3xl text-blue-600 font-satoshi">Help & Support Page</h1>
+          <h1 className="mt-20 font-satoshi text-3xl text-blue-600">
+            Help & Support Page
+          </h1>
         </div>
         <div>
-        <h3 className="text-gray-600 font-satoshi">Let us help you in your Balancer experience!</h3>
+          <h3 className="font-satoshi text-gray-600">
+            Let us help you in your Balancer experience!
+          </h3>
         </div>
         <div className="flex flex-row gap-4">
-          <HelpCard icon={data.icon[0]} title={data.title[0]} paragraph={data.paragraph[0]} />
-          <HelpCard icon={data.icon[1]} title={data.title[1]} paragraph={data.paragraph[1]} />
-          <HelpCard icon={data.icon[2]} title={data.title[2]} paragraph={data.paragraph[2]} />
+          <HelpCard
+            icon={data.icon[0]}
+            title={data.title[0]}
+            paragraph={data.paragraph[0]}
+          />
+          <HelpCard
+            icon={data.icon[1]}
+            title={data.title[1]}
+            paragraph={data.paragraph[1]}
+          />
+          <HelpCard
+            icon={data.icon[2]}
+            title={data.title[2]}
+            paragraph={data.paragraph[2]}
+          />
         </div>
       </div>
     </Layout>

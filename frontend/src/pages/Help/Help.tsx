655da8feab566ca8ef10abc641e0a2f330bb0d7a
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-02-12 16:45:26 -0500

    updated icons

--- frontend/src/pages/Help/Help.tsx
+++ frontend/src/pages/Help/Help.tsx
@@ -3,57 +3,56 @@ import { useEffect, useState } from "react";
 import HelpCard from "./HelpCard";
 
 interface HelpData {
-    title: string[];
-    paragraph: string[];
+  icon: string[];
+  title: string[];
+  paragraph: string[];
 }
 
 const HelpCardData: HelpData = {
-    title: [
-        "How To Use the Page",
-        "Feedback",
-        "How We Get Our Data"
-    ],
-    paragraph: [
-        "This is the help page for the Balancer app. "
-        + "You can use this page to learn how to use " +
-        "the Balancer app.",
-        "This is the feedback page where you can " + 
-        "give the Balancer team feedback on your" +
-        " app experience.",
-        "This is where you can learn how the" +
-        "Balancer team gets our data!"
-    ]
-}
+  icon: [
+    "UserDoctor",
+    "FeedbackIcon",
+    "MagnifyingGlassChart",
+  ],
+  title: ["How To Use this Site", "Submit Feedback", "How We Get Our Data"],
+  paragraph: [
+      "Visit this page to learn how to use the Balancer App!",
+    "Give the Balancer team feedback on your experience.",
+    "Learn where the Balancer team gets our data!",
+  ],
+};
 
 function Help() {
-    const [data, setData] = useState<HelpData>({
-        title: [
-            ""
-        ],
-        paragraph: [
-            ""
-        ]
-    }
-);
+  const [data, setData] = useState<HelpData>({
+    icon: [""],
+    title: [""],
+    paragraph: [""],
+  });
 
-useEffect(() => {
+  useEffect(() => {
     if (!data) {
-        setData({ title: ["error setting data"], paragraph: ["error setting data"] });
-    }
-    else setData(HelpCardData);
-}, [data]);
-    
+      setData({
+        icon: [""],
+        title: ["error setting data"],
+        paragraph: ["error setting data"],
+      });
+    } else setData(HelpCardData);
+  }, [data]);
+
   return (
     <Layout>
-      <div className="mt-20 px-4 flex w-full max-w-6xl flex-col items-center justify-center md:mt-28">
-      <div className="flex justify-center ">
-        <h1 className="text-blue-600 mt-20 text-3xl">Help</h1>
-    </div>
-    <div className="flex flex-row gap-4">
-       <HelpCard title={data.title[0]} paragraph={data.paragraph[0]} />
-       <HelpCard title={data.title[1]} paragraph={data.paragraph[1]} />
-       <HelpCard title={data.title[2]} paragraph={data.paragraph[2]} />
-       </div>
+      <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center px-4 md:mt-28">
+        <div className="flex justify-center ">
+          <h1 className="mt-20 text-3xl text-blue-600 font-satoshi">Help & Support Page</h1>
+        </div>
+        <div>
+        <h3 className="text-gray-600 font-satoshi">Let us help you in your Balancer experience!</h3>
+        </div>
+        <div className="flex flex-row gap-4">
+          <HelpCard icon={data.icon[0]} title={data.title[0]} paragraph={data.paragraph[0]} />
+          <HelpCard icon={data.icon[1]} title={data.title[1]} paragraph={data.paragraph[1]} />
+          <HelpCard icon={data.icon[2]} title={data.title[2]} paragraph={data.paragraph[2]} />
+        </div>
       </div>
     </Layout>
   );
--- frontend/src/pages/Help/HelpCard.tsx
+++ frontend/src/pages/Help/HelpCard.tsx
@@ -1,7 +1,11 @@
 import Layout from "../Layout/Layout";
 import { useEffect, useState } from "react";
+import MagnifyingGlassChart from "./MagnifyingGlassChart";
+import UserDoctor from "./UserDoctor";
+import FeedbackIcon from "./FeedbackIcon";
 
 interface HelpProps {
+    icon: string;
     title: string;
     paragraph: string;
 }
@@ -9,6 +13,7 @@ interface HelpProps {
 function HelpCard(data: HelpProps){
   const [paragraph, setParagraph] = useState("");
   const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState<JSX.Element | null>(null);
 
   useEffect(() => {
     if (!data.paragraph ) {
@@ -19,15 +24,23 @@ function HelpCard(data: HelpProps){
     }
         setTitle(data.title);
         setParagraph(data.paragraph);
+        if (data.icon=="UserDoctor") {
+            setIcon(<UserDoctor />);
+        } else if (data.icon=="FeedbackIcon"){
+            setIcon(<FeedbackIcon />);
+        } else if (data.icon=="MagnifyingGlassChart"){
+            setIcon(<MagnifyingGlassChart />);  
+        }
   }, [paragraph, data, title]);
 
   return (
-    <div className="h-48 flex w-full max-w-6xl flex-col items-center justify-center px-4 py-8 shadow-md md:mt-28 bg-neutral-50 rounded-xl border-2 border-blue-100 p-4">
-      <div className="p-4 h-48">
-          <h1 className="text-center text-blue-600">{title}</h1>
+    <div className="h-72 flex w-full max-w-6xl flex-col items-center justify-center px-4 shadow-md md:mt-28 bg-neutral-50 rounded-xl border-2 border-blue-100">
+      <div className="flex flex-col items-center h-48">
+      {icon && <div className="h-24 w-24">{icon}</div>}
+          <h1 className="text-center text-black py-4">{title}</h1>
           <p
             className="text-log sm:text-x; mx-auto
-          mt-5 hidden max-w-[100%] text-center font-satoshi text-gray-600 md:block"
+          hidden max-w-[100%] text-center font-satoshi text-gray-600 md:block"
           >
             {paragraph}
           </p>
@@ -35,5 +48,6 @@ function HelpCard(data: HelpProps){
     </div>
   );
 }
+
 
 export default HelpCard;

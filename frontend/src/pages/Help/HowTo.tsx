55b6d86d7c5f49b8a237e770232a9d08a6356832
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2024-02-13 00:25:41 -0500

    updated formatting

--- frontend/src/pages/Help/HowTo.tsx
+++ frontend/src/pages/Help/HowTo.tsx
@@ -35,10 +35,10 @@ function HowTo() {
   return (
     <Layout>
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center px-8">
-        <div className="mt-10 flex flex-col rounded-xl border-2 border-blue-100 bg-neutral-50 px-4 py-4 shadow-md">
-          <h3 className="flex items-center justify-center py-2 text-xl">
-            {howToData.title}
-          </h3>
+        <h3 className="mt-10 flex items-center justify-center py-4 text-xl">
+          {howToData.title}
+        </h3>
+        <div className="flex flex-col rounded-xl border-2 border-blue-100 bg-neutral-50 px-4 py-4 shadow-md">
           <div className="flex flex-col px-2 py-2">
             <p className="font-satoshi text-sm text-gray-700">
               {howToData.paragraph[0]}
@@ -50,8 +50,14 @@ function HowTo() {
           <div className="flex px-6">
             <ol>
               {howToData.instructions.map((text, index) => {
-                return <li className="font-satoshi text-sm text-gray-700 py-1"
-                key={index}>{text}</li>;
+                return (
+                  <li
+                    className="py-1 font-satoshi text-sm text-gray-700"
+                    key={index}
+                  >
+                    {text}
+                  </li>
+                );
               })}
             </ol>
           </div>

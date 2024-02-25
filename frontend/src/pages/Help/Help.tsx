5f0c6dcab20c49b309daa60dce4d8b3a69b7f032
taichan03 <tai.c.chan3@gmail.com>
2024-02-25 12:49:43 -0500

    Kristo changes

--- frontend/src/pages/Help/Help.tsx
+++ frontend/src/pages/Help/Help.tsx
@@ -4,18 +4,14 @@ import HelpCard from "./HelpCard";
 import { Link } from "react-router-dom";
 
 interface HelpData {
-    link: string[];
-    icon: string[];
-    title: string[];
-    paragraph: string[];
+  link: string[];
+  icon: string[];
+  title: string[];
+  paragraph: string[];
 }
 
 const HelpCardData: HelpData = {
-    link: [
-        "/how-to",
-        "/feedback",
-        "/data-sources"
-    ],
+  link: ["/how-to", "/feedback", "/data-sources"],
   icon: ["UserDoctor", "FeedbackIcon", "MagnifyingGlassChart"],
   title: ["How To Use this Site", "Submit Feedback", "How We Get Our Data"],
   paragraph: [
@@ -48,27 +44,28 @@ function Help() {
     <Layout>
       <div className="mt-20 flex w-full max-w-6xl flex-col items-center justify-center px-4">
         <div className="flex justify-center text-center">
-          <h1 className="mt-20 font-satoshi text-3xl text-blue-600">
+          <h2 className="mt-20 font-satoshi text-3xl text-blue-600">
             Help & Support Page
-          </h1>
+          </h2>
         </div>
-        <div className="flex text-center py-2">
+        <div className="flex py-2 text-center">
           <h3 className="font-satoshi text-gray-600">
             Let us help you in your Balancer experience!
           </h3>
         </div>
         <div className="flex flex-col gap-4 sm:flex-row">
           {data.icon.map((icon, index) => (
-            <Link to={data.link[index]} key={index} className="flex"> 
-
-            <div className="">
-            <HelpCard
+            <Link
+              to={data.link[index]}
               key={index}
-              icon={icon}
-              title={data.title[index]}
-              paragraph={data.paragraph[index]}
-            />
-            </div>
+              className="flex max-w-full flex-grow basis-1/3"
+            >
+              <HelpCard
+                key={index}
+                icon={icon}
+                title={data.title[index]}
+                paragraph={data.paragraph[index]}
+              />
             </Link>
           ))}
         </div>
--- frontend/src/pages/Help/HelpCard.tsx
+++ frontend/src/pages/Help/HelpCard.tsx
@@ -23,17 +23,31 @@ function HelpCard(data: HelpProps) {
     }
     setTitle(data.title);
     setParagraph(data.paragraph);
-    if (data.icon == "UserDoctor") {
-      setIcon(<UserDoctor />);
-    } else if (data.icon == "FeedbackIcon") {
-      setIcon(<FeedbackIcon />);
-    } else if (data.icon == "MagnifyingGlassChart") {
-      setIcon(<MagnifyingGlassChart />);
+    // if (data.icon == "UserDoctor") {
+    //   setIcon(<UserDoctor />);
+    // } else if (data.icon == "FeedbackIcon") {
+    //   setIcon(<FeedbackIcon />);
+    // } else if (data.icon == "MagnifyingGlassChart") {
+    //   setIcon(<MagnifyingGlassChart />);
+    switch (data.icon) {
+      case "UserDoctor":
+        setIcon(<UserDoctor />);
+        break;
+      case "FeedbackIcon":
+        setIcon(<FeedbackIcon />);
+        break;
+      case "MagnifyingGlassChart":
+        setIcon(<MagnifyingGlassChart />);
+        break;
+      default:
+        setIcon(null);
     }
-  }, [paragraph, data, title]);
+    // }, [paragraph, data, title]);
+  }, [data.icon, data.title, data.paragraph]);
 
   return (
-    <div className="flex h-72 w-full max-w-6xl flex-col items-center justify-center rounded-xl border-2 border-blue-100 bg-neutral-50 px-4 shadow-md md:mt-28">
+    // <div className="flex h-72 w-full max-w-6xl flex-col items-center justify-center rounded-xl border-2 border-blue-100 bg-neutral-50 px-4 shadow-md md:mt-28">
+    <div className="flex h-72 w-full min-w-[280px] max-w-6xl flex-col items-center justify-center rounded-xl border-2 border-blue-100 bg-neutral-50 px-4 shadow-md md:mt-28">
       <div className="flex h-full flex-col items-center">
         {icon && <div className="mt-6 h-24 w-24">{icon}</div>}
         <div className="mt-6 flex flex-col">

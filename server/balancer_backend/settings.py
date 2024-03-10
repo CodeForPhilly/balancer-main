3845fbc74e0dba6a3eb5f07cfcaa3e2feb5e20e0
taichan03 <tai.c.chan3@gmail.com>
2024-03-09 19:21:03 -0500

    working on JWT to set superuser

--- server/balancer_backend/settings.py
+++ server/balancer_backend/settings.py
@@ -171,6 +171,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'TOKEN_OBTAIN_SERIALIZER': 'api.models.TokenObtainPairSerializer.MyTokenObtainPairSerializer',
     'AUTH_TOKEN_CLASSES': (
         'rest_framework_simplejwt.tokens.AccessToken',
     )

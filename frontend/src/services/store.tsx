fd26fe157cbb9da9d91526f3fefcbe2d92da97e6
Tom-from-6520 <cocplayer1alpha@gmail.com>
2024-03-13 11:03:43 -0400

    change outdated front end library

--- frontend/src/services/store.tsx
+++ frontend/src/services/store.tsx
@@ -1,5 +1,5 @@
 import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import { legacy_createStore as createStore } from "redux";

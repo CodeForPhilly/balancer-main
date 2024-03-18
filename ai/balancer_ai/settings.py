d87090645b6cde2adb25867eddb285ea04c6a3ba
taichan03 <tai.c.chan3@gmail.com>
2024-03-17 23:10:41 -0400

    start api app

--- ai/balancer_ai/settings.py
+++ ai/balancer_ai/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

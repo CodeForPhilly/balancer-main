05c1606c4c185f4eb0f680b32d69875b3ed62e20
taichan03 <tai.c.chan3@gmail.com>
2024-03-19 08:16:21 -0400

    add embeddings endpoints

--- ai/api/views/embeddings/urls.py
+++ ai/api/views/embeddings/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import ExtractEmbeddingsAPIView
+from .views import StoreEmbeddingsAPIView
 
 urlpatterns = [
-    path('api/extractEmbeddings/', ExtractEmbeddingsAPIView.as_view(),
-         name='extract-embeddings'),
+    path('api/embeddings/extract_embeddings', ExtractEmbeddingsAPIView.as_view(),
+         name='extract_embeddings'),
+
+    path('api/embeddings/store_embeddings', StoreEmbeddingsAPIView.as_view(),
+         name='store_embeddings'),
 ]
--- ai/api/views/embeddings/views.py
+++ ai/api/views/embeddings/views.py
@@ -28,3 +28,27 @@ class ExtractEmbeddingsAPIView(APIView):
             # Print the error and return a response indicating an internal server error
             print(f"An error occurred: {e}")
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class StoreEmbeddingsAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            # Load the model - consider moving this outside the request handling
+            # to load it only once if performance is an issue.
+            model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+            # Extract the message from the request body
+            message_data = request.data.get('message', None)
+            if message_data is None:
+                return Response({"error": "No message provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Encode the message to get the embeddings
+            # Make sure it's a list even for a single sentence
+            embedding = model.encode([message_data])
+
+            # Return the embedding. Note: embedding is a numpy array so it needs to be converted to a list to be JSON serializable
+            return Response({"embeddings": embedding.tolist()}, status=status.HTTP_200_OK)
+        except Exception as e:
+            # Print the error and return a response indicating an internal server error
+            print(f"An error occurred: {e}")
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

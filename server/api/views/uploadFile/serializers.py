fc8c5e54a93302d60013159c76c8ea35354e4c4d
Ryan R Rogers <ryan.rogers.developer@gmail.com>
2024-03-20 13:00:47 -0400

    finish basic file uploading + serialization

--- server/api/views/uploadFile/serializers.py
+++ server/api/views/uploadFile/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import UploadFile
+import uuid
+import base64
+
 
 class UploadFileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UploadFile
-        exclude = ['id', 'guid', 'analyzed', 'approved', 'file']
+        exclude = ["id", "guid", "analyzed", "approved"]
--- server/api/views/uploadFile/views.py
+++ server/api/views/uploadFile/views.py
@@ -1,23 +1,42 @@
-
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-    
+
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+import json
+import base64
 from .models import UploadFile
 from .serializers import UploadFileSerializer
 
-@method_decorator(csrf_exempt, name='dispatch')
+
+@method_decorator(csrf_exempt, name="dispatch")
 class UploadFile(APIView):
-    authentication_classes = []
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, format=None):
-        serializer = UploadFileSerializer(data=request.data)
-        file_obj = request.data['file']
+        if not request.user.is_superuser:
+            return Response(
+                {"message": "Error, user is not a superuser."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        acceptable_file_types = ["pdf"]
+        data = json.loads(request.data["data"])
+        serializer = UploadFileSerializer(data=data)
         if serializer.is_valid():
-            #serializer.save()
-            return Response({'content': serializer.data, 'file': file_obj}, status=status.HTTP_201_CREATED)
+            if serializer.validated_data["file_type"] in acceptable_file_types:
+                print(serializer.validated_data)
+                serializer.validated_data["file"] = request.FILES["file"].read()
+                serializer.save()
+                return Response(
+                    {"content": serializer.data}, status=status.HTTP_201_CREATED
+                )
+            else:
+                return Response(
+                    {
+                        "message": f'Error. Incompatible file type. Acceptable types: {", ".join(str(i) for i in acceptable_file_types)}.'
+                    }
+                )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

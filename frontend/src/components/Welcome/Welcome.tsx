352a9d9583520820fe6cdf9768629255eb94672f
StubberG3 <stubberg3@gmail.com>
2024-03-30 15:03:23 -0400

    style: Responsive alignment to welcome header; update alignment of patient manager description and enter patient details box

--- frontend/src/components/Welcome/Welcome.tsx
+++ frontend/src/components/Welcome/Welcome.tsx
@@ -1,27 +1,27 @@
 import { ReactElement } from 'react';
 
 interface WelcomeProps {
-  subHeader?: string;
-  descriptionText?: string;
-	descriptionEl?: ReactElement;
+    subHeader?: string;
+    descriptionText?: string;
+    descriptionEl?: ReactElement;
 }
 
 function Welcome({
-  subHeader = "Designed to assist prescribers",
-  descriptionText,
-	descriptionEl,
+    subHeader = 'Designed to assist prescribers',
+    descriptionText,
+    descriptionEl,
 }: WelcomeProps) {
-  return (
-    <div className="md:mt-10">
-      <h1 className="text-center text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl md:mt-16"></h1>
-      {subHeader && (
-        <h2 className="px-2  text-center font-quicksand text-2xl font-medium text-gray-800 md:mt-7 md:p-0 md:px-0 md:text-5xl">
-          {subHeader}
-        </h2>
-      )}
-      { (descriptionText && <p className="desc1">{descriptionText}</p>) || descriptionEl}
-    </div>
-  );
+    return (
+        <div className="welcome-header w-[90%] md:w-[75%]">
+            {subHeader && (
+                <h2 className="lg:text-5x1 py-4 text-center font-quicksand text-2xl font-medium text-gray-800 md:mt-4 md:text-3xl">
+                    {subHeader}
+                </h2>
+            )}
+            {(descriptionText && <p className="desc1">{descriptionText}</p>) ||
+                descriptionEl}
+        </div>
+    );
 }
 
 export default Welcome;

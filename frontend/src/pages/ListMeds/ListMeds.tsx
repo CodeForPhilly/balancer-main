9fb0c6b5b065fb0253c206f04859d8ac257f7e74
Alec Livaditis <aleclivaditis@gmail.com>
2024-04-23 00:14:09 -0400

    get meds from database

--- frontend/src/pages/ListMeds/ListMeds.tsx
+++ frontend/src/pages/ListMeds/ListMeds.tsx
@@ -1,11 +1,28 @@
 import Layout from "../Layout/Layout";
 import Welcome from "../../components/Welcome/Welcome";
-import { MedData, medicationsInfo } from "./MedTypes";
-import { useState } from "react";
+import HourglassSpinner from "../../components/HourglassSpinner/HourglassSpinner";
+import { MedData } from "./MedTypes";
+import { useState, useEffect } from "react";
 
 function ListMeds () {
 
-  const [medications, setMedications] = useState<MedData[]>(medicationsInfo)
+  const [medications, setMedications] = useState<MedData[]>([])
+
+  useEffect(() => {
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
+    
+    fetch(`${baseUrl}/V1/medications/`)
+      .then(res => res.json())
+      .then(data => {
+        const details = data.map((med: {name: string}) => {
+        return fetch(`${baseUrl}/V1/medications/?name=${med.name}`)
+          .then(res => res.json())
+        })
+
+        return Promise.all(details)
+      })
+      .then(listWithDetails => setMedications(listWithDetails))
+  }, [])
 
   const medsList = medications.map((med) => {
     
@@ -27,7 +44,7 @@ function ListMeds () {
 
     return(
       <details>
-        <summary className="hover:cursor-pointer w-fit font-semibold leading-6 text-gray-900">{med.title}</summary>
+        <summary className="hover:cursor-pointer w-fit font-semibold leading-6 text-gray-900">{med.name}</summary>
         <div className="flex ml-6 mb-5">
           <div className="flex-1">
             <h3 className="mb-4 mt-4 text-sm font-medium text-indigo-600">
@@ -54,11 +71,13 @@ function ListMeds () {
     <Layout>
       <div className="mt-24 mx-auto w-full max-w-6xl">
         <Welcome subHeader="Medications" descriptionText="Check out the benefits and risks of medications."/>
+        {medications.length > 0 ?
         <div className="mx-auto mt-0 w-[90%] md:mt-4 md:w-[75%] font_body rounded-md border bg-white p-2 px-3 ring-1 hover:ring-slate-300 mt-4 md:mt-0 md:p-4 md:px-8">
           <div className="space-y-4 mb-6">
             {medsList}
           </div>
         </div>
+        : <div className="mt-6 flex justify-center items-center"><HourglassSpinner/></div>}
       </div>
     </Layout>
   )

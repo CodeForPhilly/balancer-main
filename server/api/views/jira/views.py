fa794a2022635af9e4cc3606e0e2f1d6a8fd570b
Sameer N <sameer4363@gmail.com>
2024-07-19 11:02:20 -0400

    Add import to fix build error

--- server/api/views/jira/views.py
+++ server/api/views/jira/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django import forms
 import requests
 import json

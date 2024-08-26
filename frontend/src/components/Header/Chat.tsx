c7826c9fe35fe9112b4d2594c74a4109ca1e9c5b
Sameer <sameer4363@gmail.com>
2024-08-26 14:48:46 -0400

    Add delete conversation button

--- frontend/src/components/Header/Chat.tsx
+++ frontend/src/components/Header/Chat.tsx
@@ -10,6 +10,7 @@ import {
   continueConversation,
   newConversation,
   updateConversationTitle,
+  deleteConversation,
 } from "../../api/apiClient";
 import ErrorMessage from "../ErrorMessage";
 
@@ -281,6 +282,7 @@ const Chat: React.FC<ChatDropDownProps> = ({ showChat, setShowChat }) => {
                 onSelectConversation={handleSelectConversation}
                 onNewConversation={handleNewConversation}
                 onSubmitConversationTitle={updateConversationTitle}
+                onDeleteConversation={deleteConversation}
               />
             ) : (
               <div className="font_body mt-6 flex flex-grow flex-col space-y-2 p-5 pb-44">
@@ -390,12 +392,14 @@ interface ConversationListProps {
   ) => Promise<
     { status: string; title: Conversation["title"] } | { error: string }
   >;
+  onDeleteConversation: (conversationId: Conversation["id"]) => void;
 }
 const ConversationList: React.FC<ConversationListProps> = ({
   conversations,
   onSelectConversation,
   onNewConversation,
   onSubmitConversationTitle,
+  onDeleteConversation,
 }) => {
   const [isEditingTitle, setIsEditingTitle] = useState<
     Conversation["id"] | null
@@ -432,6 +436,26 @@ const ConversationList: React.FC<ConversationListProps> = ({
       console.error("Error updating conversation title: ", error);
     } finally {
       setIsEditingTitle(null);
+    }
+  };
+
+  const handleDeleteConversation = async (
+    conversationId: Conversation["id"],
+    conversationTitle: Conversation["title"],
+  ) => {
+    try {
+      if (
+        window.confirm(
+          `Are you sure you want to delete the "${conversationTitle}" conversation?`,
+        )
+      ) {
+        await onDeleteConversation(conversationId);
+        setLocalConversations(
+          localConversations.filter((convo) => convo.id !== conversationId),
+        );
+      }
+    } catch (error) {
+      console.error(`Error deleting conversation ${conversationId}: `, error);
     }
   };
 
@@ -475,7 +499,7 @@ const ConversationList: React.FC<ConversationListProps> = ({
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 cursor-pointer"
-                viewBox="0 0 20 20"
+                viewBox="0 0 512 512"
                 fill="currentColor"
                 onClick={(e) => {
                   e.stopPropagation(); // Prevents the click event from bubbling up to the <li> element
@@ -486,7 +510,25 @@ const ConversationList: React.FC<ConversationListProps> = ({
                   } else handleTitleSubmit(conversation.id, newTitle);
                 }}
               >
-                <path d="M17.414 2.586a2 2 0 010 2.828l-9.586 9.586a1 1 0 01-.293.207l-4 2a1 1 0 01-1.37-1.37l2-4a1 1 0 01.207-.293l9.586-9.586a2 2 0 012.828 0zM15.707 4L13 6.707 7.707 1.414 10.414 4H15.707zM3 12.414L8.293 7.121 12.879 11.707 7.586 17H4v-3.586l-.293-.293L3 12.414z" />
+                <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+              </svg>
+
+              {/* Trash Icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 cursor-pointer"
+                viewBox="0 0 448 512"
+                fill="currentColor"
+                onClick={(e) => {
+                  e.stopPropagation(); // Prevents the click event from bubbling up to the <li> element
+                  handleDeleteConversation(conversation.id, conversation.title);
+                }}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+                  clipRule="evenodd"
+                />
               </svg>
             </div>
           </li>

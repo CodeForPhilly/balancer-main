4595584624952384062a6de24fc180cbe9950533
Vince <vchiodo15@gmail.com>
2025-02-23 13:42:34 -0500

    frontend delete med fx

--- server/api/views/medRules/urls.py
+++ server/api/views/medRules/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MedRules
+from .views import MedRules, ListOrDetailMedication
 
 urlpatterns = [
     path("v1/api/medRules",
          MedRules.as_view(), name="medRules"),
+             path('v1/api/get_full_list_med2', ListOrDetailMedication.as_view(),
+         name='list_or_detail_medication'),
+
 ]
--- server/api/views/medRules/views.py
+++ server/api/views/medRules/views.py
@@ -6,6 +6,8 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from ...models.model_medRule import MedRule
 from .serializers import MedRuleSerializer  # You'll need to create this
+from ..listMeds.models import Medication
+from ..listMeds.serializers import MedicationSerializer
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -13,16 +15,32 @@ class MedRules(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        # Get all med rules
-        med_rules = MedRule.objects.all()
+        print("test")
+        name_query = request.query_params.get('name', None)
+        if name_query:
+            try:
+                medication = Medication.objects.get(name=name_query)
+                serializer = MedicationSerializer(medication)
+                return Response(serializer.data)
+            except Medication.DoesNotExist:
+                return Response({'error': 'Medication not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            medications = Medication.objects.all()
+            serializer = MedicationSerializer(medications, many=True)
+            return Response(serializer.data)
 
-        # Serialize the data
-        serializer = MedRuleSerializer(med_rules, many=True)
+@method_decorator(csrf_exempt, name='dispatch')
+class ListOrDetailMedication(APIView):
+    permission_classes = [IsAuthenticated]
 
-        data = {
-            'status': 'success',
-            'count': len(serializer.data),
-            'results': serializer.data
-        }
-
-        return Response(data, status=status.HTTP_200_OK)
+    def get(self, request):
+        name_query = request.query_params.get('name', None)
+        if name_query:
+            try:
+                return Response({'complete'})
+            except Medication.DoesNotExist:
+                return Response({'error': 'Medication not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            medications = Medication.objects.all()
+            serializer = MedicationSerializer(medications, many=True)
+            return Response(serializer.data)

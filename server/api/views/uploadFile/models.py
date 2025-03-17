fba9421777e41c7dad6c84f0e94e1343f424927b
Gregg Stubberfield <StubberG3@gmail.com>
2025-03-16 23:34:13 -0400

    feat #198 - Set up additional tags on backend for publication, publication date, and title

--- server/api/views/uploadFile/models.py
+++ server/api/views/uploadFile/models.py
@@ -7,16 +7,16 @@ class UploadFile(models.Model):
     id = models.AutoField(primary_key=True)
     guid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     file_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    publication = models.CharField(max_length=255)
+    publication_date = models.DateTimeField(blank=True)
     file = models.BinaryField(null=True)
-    date_of_upload = models.DateTimeField(
-        auto_now_add=True, blank=True)
+    date_of_upload = models.DateTimeField(auto_now_add=True, blank=True)
     size = models.BigIntegerField()
     page_count = models.IntegerField()
     file_type = models.CharField(max_length=50)
-    uploaded_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    uploaded_by_email = models.CharField(
-        max_length=255, blank=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uploaded_by_email = models.CharField(max_length=255, blank=True)
     source_url = models.CharField(max_length=255, blank=True, null=True)
     analyzed = models.DateTimeField(blank=True, null=True)
     approved = models.DateTimeField(blank=True, null=True)

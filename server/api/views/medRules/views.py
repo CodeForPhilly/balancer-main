d068eb8611005128ad5e3ce3d2f58156185532e9
Sahil D Shah <sahildshah1@gmail.com>
2025-04-15 12:26:43 -0400

    Add  POST  to medRules

--- server/api/views/medRules/views.py
+++ server/api/views/medRules/views.py
@@ -28,3 +28,15 @@ class MedRules(APIView):
         }
 
         return Response(data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+
+        data = request.data
+
+        serializer = MedRuleSerializer(data=data, many=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

e7605d5cabf85caac1f756f4e8d76d50a3ee8f17
Sahil D Shah <sahildshah1@gmail.com>
2025-04-17 17:06:17 -0400

    Update POST method for the medRules path

--- server/api/views/medRules/views.py
+++ server/api/views/medRules/views.py
@@ -34,15 +34,6 @@ class MedRules(APIView):
 
         data = request.data
 
-        # medications
-        # Return an error if  the query doesn't return any rows
-
-        # SELECT * FROM public.api_medication
-        # WHERE id = '1'
-
-        import pdb; pdb.set_trace()
-
-        Medication.objects.filter(id__in=data['medications']).values('id', 'name', 'benefits', 'risks')   
 
         # sources
         # Return an error if the query doesn't return any rows
@@ -50,23 +41,58 @@ class MedRules(APIView):
         # SELECT id, guid FROM public.api_uploadfile
         # WHERE guid = '50c05484-d905-4383-9e5a-d8b62743afb6'
 
-        UploadFile.objects.filter(guid=data['file_guid']).values('id', 'guid') 
+        if not UploadFile.objects.filter(guid=data['file_guid']).exists():
+            return Response({'error': 'No upload file found for the provided GUID.'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            # Get the ID of the UploadFile object
+        
+        
+        if data['file_guid'] not in UploadFile.objects.values_list('guid', flat=True):
+            return Response({'error': 'No upload file found for the provided GUID.'}, status=status.HTTP_404_NOT_FOUND)
 
+        
+        upload_file_id = UploadFile.objects.get(guid=data['file_guid']).id
+        
 
         # # Return an error if the query doesn't return any rows
         # SELECT id FROM public.api_embeddings
         # WHERE upload_file_id =12 and chunk_number in (44,45,46)
 
-        Embeddings.objects.filter(upload_file_id=12, chunk_number__in=data['sources']).values('id')
+        if not Embeddings.objects.filter(upload_file_id=upload_file_id, chunk_number__in=data['sources']).exists(): 
+            return Response({'error': 'No embeddings found for the provided upload file ID and chunk numbers.'}, status=status.HTTP_404_NOT_FOUND) 
+        else:
+            embeddings_id = Embeddings.objects.get(upload_file_id=upload_file_id, chunk_number__in=data['sources']).values_list('id', flat=True)
 
 
-        # Insert medications and sources into the below Django model  with HTTP POST method
-        # serializer = MedRuleSerializer(data=, many=True)
+        # medications
+        # Return an error if  the query doesn't return any rows
+
+        # SELECT * FROM public.api_medication
+        # WHERE id = '1'
+
+        if not Medication.objects.filter(id__in=data['medications']).exists():
+            return Response({'error': 'No medications found for the provided IDs.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+        data = {
+            'rule_type': data['rule_type'],
+            'history_type': data['history_type'],
+            'reason': data['reason'],
+            'label': data['label'],
+            'medications_id': data['medications'],
+            'sources_ids': list(embeddings_id),
+            'explanation': data['explanation']
+        }
+
+
         
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return data
+
+        #Insert medications and sources into the below Django model  with HTTP POST method
+        serializer = MedRuleSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

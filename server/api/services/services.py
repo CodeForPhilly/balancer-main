8b0705d9d435216f717ba8cdd94566a3e66c144d
Sahil D Shah <sahildshah1@gmail.com>
2025-06-02 15:34:21 -0400

    Refactor model invocation logic with ModelFactory and handler classes

--- server/api/services/services.py
+++ server/api/services/services.py
@@ -3,142 +3,173 @@ This module contains functions to interact with different AI models
 """
 
 import time
+from abc import ABC, abstractmethod
+import os
+import logging
 
-def claude_citations(client, user_prompt, content_chunks):
-    """
-    This function interacts with the Claude model to generate a response
-    """
+import anthropic
+import openai
 
+class BaseModelHandler(ABC):
+    @abstractmethod
+    def handle_request(self, query, context):
+        pass
+
+class ClaudeHaiku35CitationsHandler(BaseModelHandler):
+    MODEL = "claude-3-5-haiku-20241022"
     # Model Pricing: https://docs.anthropic.com/en/docs/about-claude/pricing#model-pricing
-    
-    CLAUDE_HAIKU_3_5_PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.80, 
-                                                           'output': 4.00,}
+    PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.80, 'output': 4.00}
 
-    start_time = time.time()
-    message = client.messages.create(
-        model="claude-3-5-haiku-20241022",
-        max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "document",
-                        "source": {
-                            "type": "content",
-                            "content": content_chunks
+    def __init__(self):
+        self.client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
+    def handle_request(self, query, context):
+        start_time = time.time()
+        message = self.client.messages.create(
+            model=self.MODEL,
+            max_tokens=1024,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": query
                         },
-                        "citations": {"enabled": True}
-                    },
 
-                    {
-                        "type": "text",
-                        "text": user_prompt
-                    }
-                ]
-            }
-        ],
-    )
-    latency = time.time() - start_time
-    
+                        {
+                            "type": "document",
+                            "source": {
+                                "type": "content",
+                                "content": context
+                            },
+                            "citations": {"enabled": True}
+                        },
 
-    # Response Structure: https://docs.anthropic.com/en/docs/build-with-claude/citations#response-structure
+                    ]
+                }
+            ],
+        )
+        latency = time.time() - start_time
+        
 
-    text = []
-    cited_text = []
-    for content in message.to_dict()['content']:
-        text.append(content['text'])
-        if 'citations' in content.keys():
-            text.append(" ".join([f"<{citation['start_block_index']} - {citation['end_block_index']}>" for citation in content['citations']]))
-            cited_text.append(" ".join([f"<{citation['start_block_index']} - {citation['end_block_index']}> {citation['cited_text']}" for citation in content['citations']]))
+        # Response Structure: https://docs.anthropic.com/en/docs/build-with-claude/citations#response-structure
 
-    texts = " ".join(text)
-    cited_texts = " ".join(cited_text)
+        text = []
+        cited_text = []
+        for content in message.to_dict()['content']:
+            text.append(content['text'])
+            if 'citations' in content.keys():
+                text.append(" ".join([f"<{citation['start_block_index']} - {citation['end_block_index']}>" for citation in content['citations']]))
+                cited_text.append(" ".join([f"<{citation['start_block_index']} - {citation['end_block_index']}> {citation['cited_text']}" for citation in content['citations']]))
 
+        full_text = " ".join(text)
 
-
-    return texts, message.usage, CLAUDE_HAIKU_3_5_PRICING_DOLLARS_PER_MILLION_TOKENS, latency
-
-
-def gpt_4o_mini(client, user_prompt, content_chunks):
-    """
-    """
-
-    # Model Pricing: https://platform.openai.com/docs/pricing
-    GPT_4O_MINI_PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.15,
-                                                      'output': 0.60,}
-
-    start_time = time.time()
-    response = client.responses.create(
-        model="gpt-4o-mini",
-        instructions=user_prompt,
-        input=content_chunks,
-    )
-    latency = time.time() - start_time
-
-    return response.output_text, response.usage, GPT_4O_MINI_PRICING_DOLLARS_PER_MILLION_TOKENS, latency
-
-
-def gpt_41_nano(client, user_prompt, content_chunks):
-    """
-    """
-
-    # Model Pricing: https://platform.openai.com/docs/pricing
-    GPT_41_NANO_PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.10,
-                                                      'output': 0.40,}
-
-    start_time = time.time()
-    response = client.responses.create(
-        model="gpt-4.1-nano",
-        instructions=user_prompt,
-        input=content_chunks,
-    )
-    latency = time.time() - start_time
-
-    return response.output_text, response.usage, GPT_41_NANO_PRICING_DOLLARS_PER_MILLION_TOKENS, latency
+        return full_text, message.usage, self.PRICING_DOLLARS_PER_MILLION_TOKENS, latency
 
 
 
-def claude_haiku_3(client, user_prompt, content_chunks):
-    """
-    """
-
+class ClaudeHaiku3Handler(BaseModelHandler):
+    MODEL = "claude-3-haiku-20240307"
     # Model Pricing: https://docs.anthropic.com/en/docs/about-claude/pricing#model-pricing
-    CLAUDE_HAIKU_3_PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.25,
-                                                         'output': 1.25,}
-    
-    start_time = time.time()
+    PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.25, 'output': 1.25}
 
-    message = client.messages.create(
-        model="claude-3-haiku-20240307",
-        max_tokens=1024,
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "document",
-                        "source": {
-                            "type": "content",
-                            "content": content_chunks
+    def __init__(self):
+        self.client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
+    def handle_request(self, query, context):
+        start_time = time.time()
+        message = self.client.messages.create(
+            model=self.MODEL,
+            max_tokens=1024,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": query
                         },
-                        "citations": {"enabled": False}
-                    },
 
-                    {
-                        "type": "text",
-                        "text": user_prompt
-                    }
-                ]
-            }
-        ],
-    )
+                        {
+                            "type": "document",
+                            "source": {
+                                "type": "content",
+                                "content": context
+                            },
+                            "citations": {"enabled": False}
+                        },
 
-    text = []
-    for content in message.to_dict()['content']:
-        text.append(content['text'])
-    texts = " ".join(text)
+                    ]
+                }
+            ],
+        )
+        latency = time.time() - start_time
 
-    latency = time.time() - start_time
+        text = []
+        for content in message.to_dict()['content']:
+            text.append(content['text'])
+            
+        full_text = " ".join(text)
 
-    return texts, message.usage, CLAUDE_HAIKU_3_PRICING_DOLLARS_PER_MILLION_TOKENS, latency
+        return full_text, message.usage, self.PRICING_DOLLARS_PER_MILLION_TOKENS, latency
+
+
+class GPT4OMiniHandler(BaseModelHandler):
+    MODEL = "gpt-4o-mini"
+    # Model Pricing: https://platform.openai.com/docs/pricing
+    PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.15, 'output': 0.60}
+
+    def __init__(self):
+        self.client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    def handle_request(self, query, context):
+        start_time = time.time()
+        response = self.client.responses.create(
+            model=self.MODEL,
+            instructions=query,
+            input=context,
+        )
+        latency = time.time() - start_time
+
+        return response.output_text, response.usage, self.PRICING_DOLLARS_PER_MILLION_TOKENS, latency
+
+
+
+class GPT41NanoHandler(BaseModelHandler):
+    MODEL = "gpt-4.1-nano"
+    # Model Pricing: https://platform.openai.com/docs/pricing
+    PRICING_DOLLARS_PER_MILLION_TOKENS = {'input': 0.10, 'output': 0.40}
+
+    def __init__(self):
+        self.client = openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    def handle_request(self, query, context):
+        start_time = time.time()
+        response = self.client.responses.create(
+            model=self.MODEL,
+            instructions=query,
+            input=context,
+        )
+        latency = time.time() - start_time
+
+        return response.output_text, response.usage, self.PRICING_DOLLARS_PER_MILLION_TOKENS, latency
+
+
+class ModelFactory:
+    HANDLERS = {
+        "CLAUDE_HAIKU_3_5_CITATIONS": ClaudeHaiku35CitationsHandler,
+        "CLAUDE_HAIKU_3": ClaudeHaiku3Handler,
+        "GPT_4O_MINI": GPT4OMiniHandler,
+        "GPT_41_NANO": GPT41NanoHandler,
+        }
+
+    # HANDLERS doesn't vary per instance so we can use a class method 
+    @classmethod
+    def get_handler(cls, model_name):
+        handler_class = cls.HANDLERS.get(model_name)
+        if handler_class:
+            return handler_class()
+        else:
+            logging.error(f"Unsupported model: {model_name}")
+            return None

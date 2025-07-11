e0c04795ea56f0d53cea57ca714e965461aa2234
taichan03 <tai.c.chan3@gmail.com>
2025-07-11 00:32:35 -0400

    update

--- server/api/views/medRules/serializers.py
+++ server/api/views/medRules/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from ...models.model_medRule import MedRule
+from ...models.model_medRule import MedRule, MedRuleSource
+from ..listMeds.models import Medication
 from ..listMeds.serializers import MedicationSerializer
 from ...models.model_embeddings import Embeddings
-from ..listMeds.models import Medication
+
 
 class EmbeddingsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,21 +11,39 @@ class EmbeddingsSerializer(serializers.ModelSerializer):
         fields = ['guid', 'name', 'text', 'page_num', 'chunk_number']
 
 
+class MedicationWithSourcesSerializer(serializers.Serializer):
+    medication = MedicationSerializer()
+    sources = EmbeddingsSerializer(many=True)
+
+
 class MedRuleSerializer(serializers.ModelSerializer):
-    medications = MedicationSerializer(many=True, read_only=True)
-    medication_ids = serializers.PrimaryKeyRelatedField(
-        many=True, write_only=True, queryset=Medication.objects.all(), source='medications'
-    )
-    sources = EmbeddingsSerializer(many=True, read_only=True)
-    source_ids = serializers.PrimaryKeyRelatedField(
-        many=True, write_only=True, queryset=Embeddings.objects.all(), source='sources'
-    )
+    medication_sources = serializers.SerializerMethodField()
 
     class Meta:
         model = MedRule
         fields = [
-            'id', 'rule_type', 'history_type', 'reason', 'label',
-            'medications', 'medication_ids',
-            'sources', 'source_ids',
-            'explanation'
+            'id', 'rule_type', 'history_type', 'reason', 'label', 'explanation',
+            'medication_sources'
+        ]
+
+    def get_medication_sources(self, obj):
+
+        medrule_sources = MedRuleSource.objects.filter(
+            medrule=obj).select_related('medication', 'embedding')
+
+        med_to_sources = {}
+        for ms in medrule_sources:
+            if ms.medication.id not in med_to_sources:
+                med_to_sources[ms.medication.id] = {
+                    'medication': ms.medication,
+                    'sources': []
+                }
+            med_to_sources[ms.medication.id]['sources'].append(ms.embedding)
+
+        return [
+            {
+                'medication': MedicationSerializer(data['medication']).data,
+                'sources': EmbeddingsSerializer(data['sources'], many=True).data
+            }
+            for data in med_to_sources.values()
         ]

63bb95238c09529e8769c7e331c6aab79cd7979c
Gregg Stubberfield <StubberG3@gmail.com>
2025-08-13 23:53:16 -0400

    feat: #336, #332 - Use darker font for subtext

--- frontend/src/pages/Feedback/Feedback.tsx
+++ frontend/src/pages/Feedback/Feedback.tsx
@@ -8,7 +8,7 @@ function Feedback() {
         <div className="mt-10">
           <h2 className="px-2 text-center font-quicksand text-2xl font-medium text-gray-800 md:mt-7 md:p-0 md:px-0 md:text-5xl">Feedback</h2>
           <p className="mx-auto mt-5 mb-10
-          max-w-[100%] text-center font-satoshi text-log text-gray-400 sm:text-x; md:block">
+          max-w-[100%] text-center font-satoshi text-log text-gray-800 sm:text-x; md:block">
             Leave feedback for the Balancer Team in the form below or send us a message directly at {" "}
             <a href="mailto:balancerteam@codeforphilly.org" className="underline hover:border-blue-600 hover:text-blue-600 hover:no-underline">
               balancerteam@codeforphilly.org

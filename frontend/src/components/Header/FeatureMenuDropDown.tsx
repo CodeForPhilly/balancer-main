649f214a075cafcacb916da6d61fa28f922a1332
Gregg Stubberfield <StubberG3@gmail.com>
2025-08-14 23:00:34 -0400

    bugfix: #331 - Reapply commit 82da7a3 but without environment changes

--- frontend/src/components/Header/FeatureMenuDropDown.tsx
+++ frontend/src/components/Header/FeatureMenuDropDown.tsx
@@ -1,21 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const FeatureMenuDropDown = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <div className="h-50 absolute top-full mb-2 mt-2 flex  flex-row items-center rounded-lg border-2 bg-white font-inter text-sm sm:px-6 md:px-8 lg:px-8 xl:px-6 ">
       <div className="mx-3 my-5 ">
         <Link to="/AdminPortal">
-          <ul className=" cursor-pointer  rounded-lg p-3 transition duration-300 hover:bg-gray-100">
-            <span className=" font-bold text-black  ">Manage files</span>
+          <ul className={currentPath === "/AdminPortal" ? "subheader-nav-item subheader-nav-item-selected" : "subheader-nav-item"}>
+            <span className="font-bold">Manage files</span>
 
             <div className="mt-1 font-satoshi text-sm text-gray-400">
-              Mange and chat with files.
+              Manage and chat with files
             </div>
           </ul>
         </Link>
         <Link to="/rulesmanager">
-          <ul className=" cursor-pointer  rounded-lg p-3 transition duration-300 hover:bg-gray-100">
-            <span className=" font-bold text-black  ">Manage rules</span>
+          <ul className={currentPath === "/rulesmanager" ? "subheader-nav-item subheader-nav-item-selected" : "subheader-nav-item"}>
+            <span className="font-bold">Manage rules</span>
 
             <div className="mt-1 font-satoshi text-sm text-gray-400">
               Manage list of rules
@@ -23,15 +25,15 @@ export const FeatureMenuDropDown = () => {
           </ul>
         </Link>
         <Link to="/ManageMeds">
-          <ul className=" cursor-pointer  rounded-lg p-3 transition duration-300 hover:bg-gray-100">
-            <span className=" font-bold text-black  ">Manage meds</span>
+          <ul className={currentPath === "/ManageMeds" ? "subheader-nav-item subheader-nav-item-selected" : "subheader-nav-item"}>
+            <span className="font-bold">Manage meds</span>
 
             <div className="mt-1 font-satoshi text-sm text-gray-400">
               Manage list of meds
             </div>
           </ul>
         </Link>
-        {/* 
+        {/*
         <Link to="/drugSummary">
           <ul className=" cursor-pointer rounded-lg p-3 transition duration-300 hover:bg-gray-100">
             <span className=" font-bold text-black ">Manage Prompts</span>

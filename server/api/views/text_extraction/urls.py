5c077e0df07696aee29901b4a4ab8dd22ab8af82
Sahil D Shah <sahildshah1@gmail.com>
2025-10-01 18:19:51 -0400

    Remove missing import and URL pattern

--- server/api/views/text_extraction/urls.py
+++ server/api/views/text_extraction/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RuleExtractionAPIView, RuleExtractionAPIOpenAIView
+from .views import RuleExtractionAPIOpenAIView
 
 
 urlpatterns = [
-
-    path('v1/api/rule_extraction', RuleExtractionAPIView.as_view(),
-         name='rule_extraction'),
-    path('v1/api/rule_extraction_openai', RuleExtractionAPIOpenAIView.as_view(),
-         name='rule_extraction_openai')
+    path(
+        "v1/api/rule_extraction_openai",
+        RuleExtractionAPIOpenAIView.as_view(),
+        name="rule_extraction_openai",
+    )
 ]

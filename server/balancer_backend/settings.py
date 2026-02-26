332af9fca17085196bdad7db35d7fbe46c981274
anishamahuli <anisha.mahuli@gmail.com>
2026-02-26 16:57:36 -0500

    drf-spectacular configuration

--- server/balancer_backend/settings.py
+++ server/balancer_backend/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "djoser",
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -195,8 +196,15 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Balancer API',
+    'DESCRIPTION': 'API for the Balancer medication decision support tool',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),

bbf1034d065774a252bee3cae07300584e84fbe6
anishamahuli <anisha.mahuli@gmail.com>
2026-02-26 15:48:51 -0500

    Fixed wrong API url path in handleDownload

--- frontend/src/pages/Files/ListOfFiles.tsx
+++ frontend/src/pages/Files/ListOfFiles.tsx
@@ -61,7 +61,7 @@ const ListOfFiles: React.FC<{ showTable?: boolean }> = ({
   const handleDownload = async (guid: string, fileName: string) => {
     try {
       setDownloading(guid);
-      const { data } = await publicApi.get(`/v1/api/uploadFile/${guid}`, { responseType: 'blob' });
+      const { data } = await publicApi.get(`/api/v1/api/uploadFile/${guid}`, { responseType: 'blob' });
 
       const url = window.URL.createObjectURL(new Blob([data]));
       const link = document.createElement("a");

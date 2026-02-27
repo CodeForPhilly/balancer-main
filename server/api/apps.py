5ce77823c25f0a45fbcdc828e13214b515496bc2
Sahil D Shah <sahildshah1@gmail.com>
2026-02-27 16:09:05 -0500

    Preload SentenceTransformer model at Django startup before traffic is routed to the application instance

--- server/api/apps.py
+++ server/api/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        from .services.sentencetTransformer_model import TransformerModel
+        TransformerModel.get_instance()

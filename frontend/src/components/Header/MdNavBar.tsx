d897194f9e958b5dc3d82cf98f0f43f6d02bc8c1
Charlie Tonneslan <cst0520@gmail.com>
2026-05-27 21:37:31 -0400

    Fix mobile nav 'Medication Suggester' link target and label
    Closes #494. The mobile nav 'Medical Suggester' link was pointing at
    /login (which kicked users to the login page) and used the wrong word.
    Point it at / (where the suggester lives, matching the desktop Header)
    and rename to 'Medication Suggester' for consistency with everywhere
    else in the app.
    
    Signed-off-by: Charlie Tonneslan <cst0520@gmail.com>

--- frontend/src/components/Header/MdNavBar.tsx
+++ frontend/src/components/Header/MdNavBar.tsx
@@ -89,10 +89,10 @@ const MdNavBar = (props: LoginFormProps) => {
                     </li>
                     <li className="border-b border-gray-300 p-4">
                         <Link
-                            to="/login"
+                            to="/"
                             className="mr-9 text-black hover:border-b-2 hover:border-blue-600 hover:text-black hover:no-underline"
                         >
-                            Medical Suggester
+                            Medication Suggester
                         </Link>
                     </li>
                     <li className="border-b border-gray-300 p-4">
